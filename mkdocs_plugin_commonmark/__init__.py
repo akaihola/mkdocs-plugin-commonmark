201b14684845894fa4111b7fb5eaa79bcdbf87be
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-08-19 21:04:40 +0300

    Make monkeypatch compatible with mkdocs 1.4.0-1.6.x

--- mkdocs_plugin_commonmark/__init__.py
+++ mkdocs_plugin_commonmark/__init__.py
@@ -1,9 +1,8 @@
+import markdown
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import Files
-from mkdocs.structure.pages import (
-    Page, _ExtractTitleTreeprocessor, _RelativePathTreeprocessor
-)
+from mkdocs.structure.pages import Page
 
 from mkdocs.structure.toc import get_toc
 from .mistletoe_interop import MarkdownInterop
@@ -13,33 +12,18 @@ def render(self, config: MkDocsConfig, files: Files) -> None:
     """
     Convert the Markdown source file to HTML as per the config.
     """
-    if self.markdown is None:
-        raise RuntimeError("`markdown` field hasn't been set (via `read_source`)")
-
-    md = MarkdownInterop(
-        extensions=config['markdown_extensions'],
-        extension_configs=config['mdx_configs'] or {},
-    )
-
-    relative_path_ext = _RelativePathTreeprocessor(self.file, files, config)
-    relative_path_ext._register(md)
-
-    extract_title_ext = _ExtractTitleTreeprocessor()
-    extract_title_ext._register(md)
-
-    self.content = md.convert(self.markdown)
-    self.toc = get_toc(getattr(md, 'toc_tokens', []))
-    self._title_from_render = extract_title_ext.title
+    original_markdown = markdown.Markdown
+    markdown.Markdown = MarkdownInterop
+    self._original_render(config, files)
+    markdown.Markdown = original_markdown
 
 # Page.render = render
 
 class CommonMark(BasePlugin):
-    original_render = None
-
     def on_pre_build(self, config, **kwargs):
-        if not self.original_render:
-            self.original_render = Page.render
+        if not hasattr(Page, "_original_render"):
+            Page._original_render = Page.render
         Page.render = render
 
     def on_post_build(self, config, **kwargs):
-        Page.render = self.original_render
+        Page.render = Page._original_render
