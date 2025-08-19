2b1d1cf6fb76b15966c906515c8a94fc33f1622b
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-08-19 20:57:17 +0300

    Support mkdocs==1.4.x and Markdown==3.3.x

--- mkdocs_plugin_commonmark/__init__.py
+++ mkdocs_plugin_commonmark/__init__.py
@@ -1,26 +1,35 @@
+from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
-from mkdocs.structure.pages import Page, _RelativePathExtension
+from mkdocs.structure.files import Files
+from mkdocs.structure.pages import (
+    Page, _ExtractTitleTreeprocessor, _RelativePathTreeprocessor
+)
 
 from mkdocs.structure.toc import get_toc
 from .mistletoe_interop import MarkdownInterop
 
 
-def render(self, config, files):
+def render(self, config: MkDocsConfig, files: Files) -> None:
     """
     Convert the Markdown source file to HTML as per the config.
     """
-
-    extensions = [
-        _RelativePathExtension(self.file, files)
-    ] + config['markdown_extensions']
+    if self.markdown is None:
+        raise RuntimeError("`markdown` field hasn't been set (via `read_source`)")
 
     md = MarkdownInterop(
-        extensions=extensions,
-        extension_configs=config['mdx_configs'] or {}
+        extensions=config['markdown_extensions'],
+        extension_configs=config['mdx_configs'] or {},
     )
+
+    relative_path_ext = _RelativePathTreeprocessor(self.file, files, config)
+    relative_path_ext._register(md)
+
+    extract_title_ext = _ExtractTitleTreeprocessor()
+    extract_title_ext._register(md)
 
     self.content = md.convert(self.markdown)
     self.toc = get_toc(getattr(md, 'toc_tokens', []))
+    self._title_from_render = extract_title_ext.title
 
 # Page.render = render
 
